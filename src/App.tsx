8f8e5b961b00b4c1a4de254d46ede3aced67f74e
Mingyang-Li <mingyangli118@gmail.com>
2021-07-03 11:46:40 +1200

    store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import GlobalContext from './contexts/GlobalContext';
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 
 const App: React.FC = () => {
+  const store = {"key": "value"};
   return (
     <div className="App">
-      <p>starter</p>
+      <GlobalContext.Provider value={store}>
+        <MuiThemeProvider theme={store}>
+          <p>App.js</p>
+        </MuiThemeProvider>
+      </GlobalContext.Provider>
     </div>
   );
 }
---
+++ src/contexts/GlobalContext.ts
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const GlobalContext = createContext({"key": "value"});
+export default GlobalContext;
